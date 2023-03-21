e4e22342d374d10796e91be9b0c3125b7ec67965
Vahe Abovyan <vahe.abov@gmail.com>
2023-03-21 20:22:09 +0400

    add sendmail and user feedback

--- src/assets/js/modules/form.js
+++ src/assets/js/modules/form.js
@@ -3,16 +3,53 @@ const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const messageInput = document.querySelector('#message');
 const errorSpans = document.querySelectorAll('.error');
+const spinner = document.querySelector('.spinner');
+const submitBtn = document.querySelector('[data-btn="submit"]');
+const toast = document.querySelector('.toast');
+
+const API_URL =
+  'https://portfolio-back-production-323a.up.railway.app/sendmail';
 
 let errors = 0;
 
-form.addEventListener('submit', (event) => {
+function showToast(msg, timeout) {
+  toast.textContent = msg;
+  toast.classList.add('show');
+
+  setTimeout(() => {
+    toast.classList.remove('show');
+    toast.textContent = '';
+  }, timeout);
+}
+
+form.addEventListener('submit', async (event) => {
   event.preventDefault();
-  console.log(validateInputs());
   if (validateInputs() === 0) {
     form.classList.remove('invalid');
-    console.log('submitted');
-    form.reset();
+    try {
+      spinner.style.display = 'block';
+      submitBtn.classList.add('disabled');
+      const res = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: nameInput.value,
+          email: emailInput.value,
+          message: messageInput.value
+        })
+      });
+      const data = await res.json();
+      showToast(data.msg, 3000);
+      form.reset();
+      spinner.style.display = 'none';
+      submitBtn.classList.remove('disabled');
+    } catch (error) {
+      spinner.style.display = 'none';
+      submitBtn.classList.remove('disabled');
+      alert(error.message);
+    }
   } else {
     form.classList.add('invalid');
   }
