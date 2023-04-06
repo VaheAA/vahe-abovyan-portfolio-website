f3685f2ff6228799541e7b8e61f024c9ccb2ea1e
Vahe Abovyan <vahe.abov@gmail.com>
2023-04-06 19:42:47 +0400

    fix slider pagination

--- src/assets/js/modules/swiper.js
+++ src/assets/js/modules/swiper.js
@@ -1,34 +1,39 @@
 // core version + navigation, pagination modules:
-import Swiper, { Navigation } from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
 // import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-  modules: [Navigation],
+  modules: [Navigation, Pagination],
   direction: 'horizontal',
   slidesPerView: 3,
   spaceBetween: 25,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
   breakpoints: {
     320: {
       slidesPerView: 1,
-      spaceBetween: 20
+      spaceBetween: 20,
     },
     780: {
       slidesPerView: 3,
-      spaceBetween: 25
-    }
+      spaceBetween: 25,
+    },
   },
 
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
+    prevEl: '.swiper-button-prev',
   },
 
   // And if we need scrollbar
   scrollbar: {
-    el: '.swiper-scrollbar'
-  }
+    el: '.swiper-scrollbar',
+  },
 });
